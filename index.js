60f5554145bb440f216dbd432c18dc9dd9cb5c71
Alejandro Sanguino Osa <116163899+sanguino09@users.noreply.github.com>
2023-02-22 03:23:17 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -2,17 +2,13 @@
 function validate() {
   if (document.getElementById("remember").checked) {
     console.log("checked");
-    document
-      .getElementById("apadrinaLink")
-      .setAttribute("href", "./index.html");
+    document.getElementById("apadrinaLink").setAttribute("href", "./index.html");
     document.getElementById("apadrinaLink").removeAttribute("hidden", "");
-    document.getElementById("blogLink").setAttribute("href", "#");
+    document.getElementById("blogLink").setAttribute("href", "./blog.html");
     document.getElementById("blogLink").removeAttribute("hidden", "");
-    document
-      .getElementById("contactLink")
-      .setAttribute("href", "./contact.html");
+    document.getElementById("contactLink").setAttribute("href", "./contact.html");
     document.getElementById("contactLink").removeAttribute("hidden", "");
-    document.getElementById("policyLink").setAttribute("href", "#");
+    document.getElementById("policyLink").setAttribute("href", "./policy.html");
     document.getElementById("policyLink").removeAttribute("hidden", "");
   } else {
     console.log("check me out");
