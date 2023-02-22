6e5d33c8ace35b258762dc849ae5a1a3c194b419
Alejandro Sanguino Osa <116163899+sanguino09@users.noreply.github.com>
2023-02-22 01:48:37 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -2,14 +2,22 @@ function validate() {
   if (document.getElementById("remember").checked) {
     console.log("checked");
     document.getElementById("apadrinaLink").setAttribute("href","./index.html");
+    document.getElementById("apadrinaLink").removeAttribute("hidden","");
     document.getElementById("blogLink").setAttribute("href","#");
+    document.getElementById("blogLink").removeAttribute("hidden","");
     document.getElementById("contactLink").setAttribute("href","./contact.html");
+    document.getElementById("contactLink").removeAttribute("hidden","");
     document.getElementById("policyLink").setAttribute("href","#");
-  } else {
+    document.getElementById("policyLink").removeAttribute("hidden","");
+  } else{
     console.log("check me out");
     document.getElementById("apadrinaLink").setAttribute("href","#");
+    document.getElementById("apadrinaLink").setAttribute("hidden","");
     document.getElementById("blogLink").setAttribute("href","#");
+    document.getElementById("blogLink").setAttribute("hidden","");
     document.getElementById("contactLink").setAttribute("href","#");
+    document.getElementById("contactLink").setAttribute("hidden","");
     document.getElementById("policyLink").setAttribute("href","#");
+    document.getElementById("policyLink").setAttribute("hidden","");
   }
 }
