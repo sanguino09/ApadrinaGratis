4dfb49f2cbaf88f9fd1c4f4408e0f87abdf27d77
Alejandro Sanguino Osa <116163899+sanguino09@users.noreply.github.com>
2023-02-22 01:54:02 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
+document.getElementById("apadrinaLink").setAttribute("hidden", "");
+document.getElementById("blogLink").setAttribute("hidden", "");
+document.getElementById("contactLink").setAttribute("hidden", "");
+document.getElementById("policyLink").setAttribute("hidden", "");
 function validate() {
   if (document.getElementById("remember").checked) {
     console.log("checked");
-    document.getElementById("apadrinaLink").setAttribute("href","./index.html");
-    document.getElementById("apadrinaLink").removeAttribute("hidden","");
-    document.getElementById("blogLink").setAttribute("href","#");
-    document.getElementById("blogLink").removeAttribute("hidden","");
-    document.getElementById("contactLink").setAttribute("href","./contact.html");
-    document.getElementById("contactLink").removeAttribute("hidden","");
-    document.getElementById("policyLink").setAttribute("href","#");
-    document.getElementById("policyLink").removeAttribute("hidden","");
-  } else{
+    document
+      .getElementById("apadrinaLink")
+      .setAttribute("href", "./index.html");
+    document.getElementById("apadrinaLink").removeAttribute("hidden", "");
+    document.getElementById("blogLink").setAttribute("href", "#");
+    document.getElementById("blogLink").removeAttribute("hidden", "");
+    document
+      .getElementById("contactLink")
+      .setAttribute("href", "./contact.html");
+    document.getElementById("contactLink").removeAttribute("hidden", "");
+    document.getElementById("policyLink").setAttribute("href", "#");
+    document.getElementById("policyLink").removeAttribute("hidden", "");
+  } else {
     console.log("check me out");
-    document.getElementById("apadrinaLink").setAttribute("href","#");
-    document.getElementById("apadrinaLink").setAttribute("hidden","");
-    document.getElementById("blogLink").setAttribute("href","#");
-    document.getElementById("blogLink").setAttribute("hidden","");
-    document.getElementById("contactLink").setAttribute("href","#");
-    document.getElementById("contactLink").setAttribute("hidden","");
-    document.getElementById("policyLink").setAttribute("href","#");
-    document.getElementById("policyLink").setAttribute("hidden","");
+    document.getElementById("apadrinaLink").setAttribute("href", "#");
+    document.getElementById("apadrinaLink").setAttribute("hidden", "");
+    document.getElementById("blogLink").setAttribute("href", "#");
+    document.getElementById("blogLink").setAttribute("hidden", "");
+    document.getElementById("contactLink").setAttribute("href", "#");
+    document.getElementById("contactLink").setAttribute("hidden", "");
+    document.getElementById("policyLink").setAttribute("href", "#");
+    document.getElementById("policyLink").setAttribute("hidden", "");
   }
 }
