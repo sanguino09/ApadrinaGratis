38d5d20feb7d88f8d1fd3280a7f6dfb2002f8263
Alejandro Sanguino Osa <116163899+sanguino09@users.noreply.github.com>
2023-02-25 11:56:13 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 function validate() {
   if (document.getElementById("remember").checked) {
     console.log("checked");
-    document.getElementById("apadrinaLink").setAttribute("href", "./index.html");
+    document
+      .getElementById("apadrinaLink")
+      .setAttribute("href", "./index.html");
     document.getElementById("apadrinaLink").removeAttribute("hidden", "");
     document.getElementById("blogLink").setAttribute("href", "./blog.html");
     document.getElementById("blogLink").removeAttribute("hidden", "");
-    document.getElementById("contactLink").setAttribute("href", "./contact.html");
+    document
+      .getElementById("contactLink")
+      .setAttribute("href", "./contact.html");
     document.getElementById("contactLink").removeAttribute("hidden", "");
     document.getElementById("policyLink").setAttribute("href", "./policy.html");
     document.getElementById("policyLink").removeAttribute("hidden", "");
@@ -37,20 +41,16 @@ let img = true;
 function imgSwitch() {
   console.log("hola");
   if (img == true) {
-    document
-      .getElementById("images")
-      .setAttribute("style", "background-color:#232323");
-    document
-      .getElementById("video")
-      .setAttribute("style", "background-color:white");
+    document.getElementById("images").setAttribute("style", "background-color:#232323");
+    document.getElementById("imgIcon").setAttribute("style", "color:#7d7d7d");
+    document.getElementById("video").setAttribute("style", "background-color:white");
+    document.getElementById("vidIcon").setAttribute("style", "color:white;margin-left:30vw;");
     img = false;
   } else {
-    document
-      .getElementById("images")
-      .setAttribute("style", "background-color:white");
-    document
-      .getElementById("video")
-      .setAttribute("style", "background-color:#232323");
+    document.getElementById("images").setAttribute("style", "background-color:white");
+    document.getElementById("imgIcon").setAttribute("style", "color:white");
+    document.getElementById("video").setAttribute("style", "background-color:#232323");
+    document.getElementById("vidIcon").setAttribute("style", "color:#7d7d7d;margin-left:30vw;");
     img = true;
   }
 }
